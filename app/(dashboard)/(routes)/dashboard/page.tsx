22a4a827a961e71df11cc9791a8a9d8345761430
Sahil Kotian <sahil.kotian316@gmail.com>
2023-12-27 05:46:30 +0530

    Added Layout for Main Dashboard Content

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,77 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
-export default function Dashboard() {
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: "text-violet-600",
+    bgColor: "bg-violet-600/10",
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    href: '/music',
+    color: "text-emerald-600",
+    bgColor: "bg-emerald-600/10",
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-pink-600",
+    bgColor: "bg-pink-600/10",
+    href: '/image',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: "text-orange-600",
+    bgColor: "bg-orange-600/10",
+    href: '/video',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: "text-indigo-600",
+    bgColor: "bg-indigo-600/10",
+    href: '/code',
+  },
+];
+
+export default function HomePage() {
+  const router = useRouter();
   return (
     <div>
-      <p>Dashboard Page</p> 
-      <UserButton afterSignOutUrl="/"/>
+      <div className="mb-8 space-y-2">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore Brainiac's AI Brilliance
+        </h2>
+        <p className="text-muted-foreground font-normal text-sm md:text-lg text-center">
+          Engage with the Next-gen Neural Nexus - Experience Unmatched AI Brilliance
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
@@ -51,29 +52,33 @@ const routes = [
 ];
 
 export default function Sidebar() {
-  return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-emerald-700 text-white">
-      <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative h-28 md:h-20 w-[90%] md:w-full mr-4">
-            <Image fill alt="Logo" src="/assets/images/logo.png" />
-          </div>
-        </Link>
-        <div className="space-y-1">
-          {routes.map((route) => (
-            <Link
-              key={route.href} 
-              href={route.href}
-              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/20 rounded-lg transition")}
-            >
-              <div className="flex items-center flex-1">
-                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                {route.label}
-              </div>
-            </Link>
-          ))}
+    const pathname = usePathname();
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full bg-black text-white">
+            <div className="px-3 py-2 flex-1">
+                <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+                    <div className="relative h-28 md:h-20 w-[90%] md:w-full mr-4">
+                        <Image fill alt="Logo" src="/assets/images/logo.png" />
+                    </div>
+                </Link>
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link
+                            key={route.href} 
+                            href={route.href}
+                            className={cn(
+                                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/20" : "text-zinc-400",
+                            )}
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
