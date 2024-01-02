0a716fe2f2796b743e1457fb8ec201f5ce723de8
Sahil Kotian <sahil.kotian316@gmail.com>
2024-01-02 18:57:03 +0530

    Updated Logo

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,16 +3,13 @@ import Image from "next/image"
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-60 h-20 relative animate-pulse">
+      <div className="w-28 h-28 relative animate-pulse">
         <Image
           alt="Logo"
-          src="/assets/images/logo.png"
+          src="/assets/images/secondary-logo.png"
           fill
         />
       </div>
-      <p className="text-sm font-normal text-muted-foreground">
-        Brainiac is thinking...
-      </p>
     </div>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -57,7 +57,7 @@ export default function Sidebar() {
         <div className="space-y-4 py-4 flex flex-col h-full bg-black text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                    <div className="relative h-28 md:h-20 w-[90%] md:w-full mr-4">
+                    <div className="relative h-24 md:h-16 w-[90%] md:w-full mr-4">
                         <Image fill alt="Logo" src="/assets/images/logo.png" />
                     </div>
                 </Link>
